0799c55b7ac02be14d77c2bd6adf166bd469d941
한예주 <yejoo_han@college.harvard.edu>
2023-02-19 16:16:40 -0500

    implemented handling client requests on server side

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ REGISTER = 1
 LOGIN = 2
 SEND_MSG = 3
 LOGOUT = 4
+LIST_ACCOUNT = 5
 
 USERS = {}
 MESSAGES = {}
@@ -31,7 +32,7 @@ class Message():
 def create_account(username, password):
     # Fails if username is not unique
     if username in USERS:
-        return -1
+        return 1
 
     # Appends account to list of users otherwise
     USERS[username] = User(username, password)
@@ -45,14 +46,19 @@ def login(username, password):
         if USERS[username].password == hash(password):
             return 0
         else:
-            return -1
+            return 1
 
     # No matching username found. Fail.
-    return -1
+    return 2
 
 """ Function to list all accounts """
 def list_accounts():
-    return USERS
+    accounts = ""
+    for username in USERS:
+        accounts += username
+        accounts += "\n"
+
+    return accounts
 
 """ Function to send a message from one user to another """
 def send_message(sender, receiver, message):
@@ -62,7 +68,7 @@ def send_message(sender, receiver, message):
         return 0
 
     # Could not find receiver. Fail.
-    return -1
+    return 1
 
 """ Function to receive all messages on behalf of user """
 def receive_messages(receiver):
@@ -127,18 +133,63 @@ def handle_connection(conn):
             break
         # TODO: Parse data according to wire protocol and do right thing
         opcode = data[0]
+        # TODO: Integrate num_args in client side. Also add stuff about failure (i.e. if opcode is LOGIN and the # of args is 4, fail lol)
+        num_args = data[1]
+
         if opcode == PING:
             conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("PONG!"))
         elif opcode == LOGIN:
-            conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Logged In!"))
+            # Parse login arguments
+            username_length = int(data[2])
+            username = data[3: 3 + username_length].decode()
+            password_length = int(data[3 + username_length])
+            password = data[4 + username_length: 4 + username_length + password_length].decode()
+
+            # Login and send success/failure to client
+            login_status = login(username, password)
+            if login_status == 0:
+                conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Logged In!"))
+            elif login_status == 1:
+                conn.sendall((1).to_bytes(1, byteorder='big') + pack_msg("Incorrect Password"))
+            elif login_status == 2:
+                conn.sendall((1).to_bytes(1, byteorder='big') + pack_msg("Username Not Found"))
         elif opcode == REGISTER:
-            conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Registered!"))
+            # Parse register arguments
+            # TODO: redundancy so cleanup lol
+            username_length = int(data[2])
+            username = data[3: 3 + username_length].decode()
+            password_length = int(data[3 + username_length])
+            password = data[4 + username_length: 4 + username_length + password_length].decode()
+
+            # Create account and send success/failure to client
+            register_status = create_account(username, password)
+            if register_status == 0:
+                conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Registered!"))
+            elif register_status == 1:
+                conn.sendall((1).to_bytes(1, byteorder='big') + pack_msg("Username Already Exists"))
         elif opcode == SEND_MSG:
-            conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Sent Message!"))
+            # Parse send arguments
+            sender_length = int(data[2])
+            sender = data[3: 3 + sender_length].decode()
+            receiver_length = int(data[3 + sender_length])
+            receiver = data[4 + sender_length: 4 + sender_length + receiver_length].decode()
+            message_length = int(data[4 + sender_length + receiver_length])
+            message = data[5 + sender_length + receiver_length: 5 + sender_length + receiver_length + message_length]
+
+            # Send message and send success/failure to client
+            send_status = send_message(sender, receiver, message)
+            if send_status == 0:
+                conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Successfully Sent Message!"))
+            elif send_status == 1:
+                conn.sendall((1).to_bytes(1, byteorder='big') + pack_msg("Receiver Username Does Not Exist"))
+        elif opcode == LIST_ACCOUNTS:
+            accounts = list_accounts()
+            # TODO: maybe not use pack_msg on the full thing. if there are lots of users, may fail assert in it.
+            conn.sendall((0).to_bytes(1, byteorder='big')) + pack_msg(accounts)
         elif opcode == LOGOUT:
             conn.sendall((0).to_bytes(1, byteorder='big') + pack_msg("Logout Acknowledged!"))
             break
-    conn.close()
+        conn.close()
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
