d81bd9a2409e768bc0393237de0e8aa5d2ccdf43
Evan Howard <evanhoward@college.harvard.edu>
2023-03-02 14:33:17 -0500

    call tick X times per second

--- logical-clocks/model.py
+++ logical-clocks/model.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+import time, random
+
+def tick():
+
+def main():
+    clock_speed = random.randint(1, 6)
+
+    start = time.time()
+    period = 1.0 / clock_speed
+    while True:
+        if (time_time() - start) > period:
+            start += period
+            tick()
+
+main()
