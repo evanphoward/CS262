f2be1d286fa98d58a40cc7af0badf87060b7e38e
한예주 <yejoo_han@college.harvard.edu>
2023-04-08 13:06:00 -0400

    Implemented remaining server functions and unit testing

--- replication/journals/yejoo_engineering_journal.md
+++ replication/journals/yejoo_engineering_journal.md
@@ -1,3 +1,9 @@
 # 04/07
 To make replication easier, choosing to implement a Server class in [server.py] so that we can have multiple servers running (and implementing a consensus algorithm among servers later) if need be.
 Within the Server class, to guarantee that messages & accounts don't get lost, persisting accounts / message database with a CSV file.
+
+# 04/08
+Choosing to make CSV queue all messages (and have a separate function later for receiving instantly if logged in). Then on delivery, CSV will delete messages from queue.
+In addition to the two dataframes (accounts and messages), the server will also keep a dictionary of clients for ease of access & testing. The dictionary, unlike the dataframes, do not persist upon failure.
+Python's hash function only guarantees same hash in a single session, so using backups and having to run a program multiple times means hash function for password is not consistent. Removing hash for now. Can deal with it later (but also doesn't seem to be the core of the design exercise).
+Current unit tests only test for the server's version of events (i.e. self.users) and not actual persistence. Not sure if we should also try and test for persistence. (We probably should but seems like it would be quite tedious to do, so perhaps the assumption we should have is that the two are always aligned, which isn't too bad of an assumption)
--- replication/server.py
+++ replication/server.py
@@ -1,12 +1,33 @@
 import os
 import socket
 import pandas as pd
+import sys
+import fnmatch
 
 # Host IP for Server ID 1, 2, 3
 HOSTS = ["127.0.0.1", "127.0.0.1", "127.0.0.1"]
 # Port used is PORT + ID
 PORT = 65432
 
+""" Class that represents a User of the application """
+class User():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.socket = None
+        self.messages = []
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+""" Class that represents a Message on the application """
+class Message():
+    def __init__(self, sender, receiver, message):
+        self.sender = sender
+        self.receiver = receiver
+        self.message = message
+
+""" Class that represents a Server """
 class Server():
     """ Initialize Server Object """
     def __init__(self, id):
@@ -26,16 +47,23 @@ class Server():
         self.message_path = "server-" + str(self.port) + "-messages.csv"
 
         if os.path.isfile(self.account_path):
-            self.accounts = pd.read_csv(self.account_path)
+            self.accounts = pd.read_csv(self.account_path, dtype = str)
         else:
             self.accounts = pd.DataFrame(columns = ["Username", "Password", "Time"])
             self.accounts.to_csv(self.account_path, index = False)
 
         if os.path.isfile(self.message_path):
-            self.messages = pd.read_csv(self.message_path)
+            self.messages = pd.read_csv(self.message_path, dtype = str)
         else:
             self.messages = pd.DataFrame(columns = ["Sender", "Receiver", "Message", "Time"])
             self.messages.to_csv(self.message_path, index = False)
+
+        # Create list of users for this server and parse CSV files
+        self.users = {}
+        for index, account in self.accounts.iterrows():
+            self.users[account["Username"]] = User(account["Username"], account["Password"])
+        for index, message in self.messages.iterrows():
+            self.users[message["Receiver"]].add_message(Message(message["Sender"], message["Receiver"], message["Message"]))
 
     """ Function to create a new account """
     # TODO: make sure that self.accounts is fresh before calling create_account
@@ -45,54 +73,72 @@ class Server():
             return 1
 
         # Appends account to list of users otherwise and updates CSV file
-        self.accounts.loc[len(self.accounts)] = [username, password, pd.Timestamp.now()]
+        self.users[username] = User(username, password)
+        self.accounts.loc[len(self.accounts)] = [username, self.users[username].password, pd.Timestamp.now()]
         self.accounts.to_csv(self.account_path, index = False)
         return 0
 
     """ Function to log in """
     def login(self, username, password):
-        match_username = self.accounts.loc[self.accounts["Username"] == username]
+        if username in self.users:
+            # If there is a matching username, either log them in or fail based on password
+            if self.users[username].password == password:
+                return 0
+            else:
+                return 1
 
         # No matching username found. Fail.
-        if len(match_username) == 0:
-            return 2
-
-        # Login if password matches. Fail otherwise
-        if match_username["Password"][0] == password:
-            return 0
-        else:
-            return 1
+        return 2
 
     """ Function to search and list all accounts """
-    def list_accounts(search):
-        accounts = ""
+    def list_accounts(self, search):
+        accounts = []
         # Find all matching usernames
-        for username in self.accounts["Username"]:
+        for username in self.users:
             if fnmatch.fnmatch(username, search):
-                accounts += username
-                accounts += "\n"
+                accounts.append(username)
 
         return accounts
 
-    """ Function to send a message from one user to another """
-    def send_message(sender, receiver, message):
-        # Find receiver and queue message
-        if receiver in self.accounts['Username'].values:
-            # TODO: Figure out how we are delivering to the user, what the difference is when the user is logged in and isn't logged in
-            return 0
+    """ Function to queue a message from one user to another """
+    def send_message(self, sender, receiver, message):
+        match_username = self.accounts.loc[self.accounts["Username"] == receiver]
 
-        # Could not find receiver. Fail.
-        return 1
+        # No matching receiver found. Fail.
+        if len(match_username) == 0:
+            return 1
+
+        # Queue message
+        # TODO: queueing all "sent" messages. should have another function deal with receiving messages instantly if logged in
+        self.users[receiver].add_message(Message(sender, receiver, message))
+        self.messages.loc[len(self.messages)] = [sender, receiver, message, pd.Timestamp.now()]
+        self.messages.to_csv(self.message_path, index = False)
+        return 0
 
     """ Function to receive all messages on behalf of user """
-    def receive_messages(receiver):
+    def receive_messages(self, receiver):
         # Deliver all unread messages
-        # TODO: Pending how we implement pending messages. Is the CSV cleared of all messages that match this receiver after we delvier them?
+        to_deliver = ""
+        for message in self.users[receiver].messages:
+            to_deliver += ("From " + message.sender + ": " + message.message + "\n")
+
+        # Delete messages
+        self.messages = self.messages[self.messages["Receiver"] != receiver]
+        self.messages.to_csv(self.message_path, index = False)
+        self.users[receiver].messages = []
+        return to_deliver
 
     """ Function to delete account """
-    def delete_account(username):
+    def delete_account(self, username):
         # Delete Account
-        # TODO: Delete account from dataframe and from CSV file
+        # TODO: add handling of messages to be received by deleted user
+        self.accounts = self.accounts[self.accounts["Username"] != username]
+        self.accounts.to_csv(self.account_path, index = False)
+        self.messages = self.messages[self.messages["Receiver"] != username]
+        self.messages.to_csv(self.message_path, index = False)
+        del self.users[username]
+
+        return
 
     def run(self):
         self.server.bind((self.host, self.port))
@@ -109,7 +155,64 @@ class Server():
 
 def unit_tests():
     server = Server(0)
-    server.login("yejoo", "idk")
+    # Test: Successfully creates account
+    server.create_account("yejoo", "0104")
+    assert(server.users["yejoo"].username == "yejoo")
+    assert(server.users["yejoo"].password == "0104")
+
+    # Test: Username must be unique
+    server.create_account("yejoo", "0123")
+    assert(server.users["yejoo"].username == "yejoo")
+    assert(server.users["yejoo"].password == "0104")
+
+    # Test: Listing Accounts
+    server.create_account("idk", "sth")
+    server.create_account("yej", "password")
+    server.create_account("middle", "mid")
+    assert(set(server.list_accounts("*")) == set(["yejoo", "yej", "idk", "middle"]))
+    assert(set(server.list_accounts("ye*")) == set(["yejoo", "yej"]))
+    assert(set(server.list_accounts("*oo")) == set(["yejoo"]))
+    assert(set(server.list_accounts("*d*")) == set(["idk", "middle"]))
+
+    # Test: Login only logs in users with correct passwords
+    assert(server.login("yejoo", "0123") == 1)
+    assert(server.login("yejoo", "0104") == 0)
+    assert(server.login("idk", "sth") == 0)
+    assert(server.login("dklfjsk;", "sdl k") == 2)
+    assert(server.login("middle", "idk") == 1)
+
+    # Test: sending message queues message
+    server.send_message("yejoo", "idk", "secrete")
+    server.send_message("yejoo", "idk", "dfjopadd")
+    server.send_message("idk", "yejoo", "dofjsoi")
+    sent_messages_idk = server.users["idk"].messages
+    assert(len(sent_messages_idk) == 2)
+    assert(sent_messages_idk[0].sender == "yejoo")
+    assert(sent_messages_idk[0].receiver == "idk")
+    assert(sent_messages_idk[0].message == "secrete")
+    assert(sent_messages_idk[1].sender == "yejoo")
+    assert(sent_messages_idk[1].receiver == "idk")
+    assert(sent_messages_idk[1].message == "dfjopadd")
+    sent_messages_yejoo = server.users["yejoo"].messages
+    assert(len(sent_messages_yejoo) == 1)
+    assert(sent_messages_yejoo[0].sender == "idk")
+    assert(sent_messages_yejoo[0].receiver == "yejoo")
+    assert(sent_messages_yejoo[0].message == "dofjsoi")
+
+    # Test: receiving message looks at queued message
+    assert(server.receive_messages("yejoo") == "From idk: dofjsoi\n")
+    assert(server.receive_messages("idk") == "From yejoo: secrete\nFrom yejoo: dfjopadd\n")
+
+    # Test: messages are received just once.
+    assert(server.receive_messages("yejoo") == "")
+    assert(server.receive_messages("idk") == "")
+
+    # Test: deleted account returns messages and gets rid of user
+    server.send_message("yejoo", "idk", "more")
+    server.send_message("yejoo", "idk", "more2")
+    server.delete_account("idk")
+    assert("idk" not in server.users)
+    assert(server.receive_messages("yejoo") == "")
 
 def main():
     if len(sys.argv) != 2:
